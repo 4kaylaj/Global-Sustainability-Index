cb1620e2b29355b0aaf5f6c24aee59e9534522e5
4kaylaj <98329909+4kaylaj@users.noreply.github.com>
2023-03-12 18:38:53 -0400

    Update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ console.log('Is this working?');
 let viz;
 
 //Add Share Link to Tableau Public in here
-const url = "https://public.tableau.com/views/GlobalSustainabilityIndex/Sheet1?:language=en-US&:display_count=n&:origin=viz_share_link";
+const url = "https://public.tableau.com/shared/Z28XGQSXP?:display_count=n&:origin=viz_share_link";
 
 const vizContainer = document.getElementById('vizContainer');
 const options = {
